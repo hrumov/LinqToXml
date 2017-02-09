42860a67b5f1444c76d84344ddb5487f4b3126c0
Aliaksei Parkhimovich <aliaksei_parkhimovich@epam.com>
2017-02-09 16:47:11 +0300

    0

---
+++ htLinqToXml/XmlRead.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace htLinqToXml
+{
+    public class XmlRead
+    {
+        XDocument xDoc = XDocument.Load(ResourceFile.XmlFilePath);
+    }
+}
